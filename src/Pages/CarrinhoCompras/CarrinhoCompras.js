68fa90243a5a893a0f9fff4e425f12f19c2c2764
Henrique Lutfi <henrique_lutfi@hotmail.com>
2022-06-20 19:59:56 -0300

    teste post pedido pagina carrinho

--- src/Pages/CarrinhoCompras/CarrinhoCompras.js
+++ src/Pages/CarrinhoCompras/CarrinhoCompras.js
@@ -1,29 +1,60 @@
-import React, { useContext, useState } from 'react'
-import { BarraPesquisa } from '../../Components/BarraPesquisa/BarraPesquisa'
-import { Fundo } from '../../Components/BarraPesquisa/Style'
-import { CartContext } from '../../Contexts/CartContext'
+import React, { useContext, useState } from "react";
+import { BarraPesquisa } from "../../Components/BarraPesquisa/BarraPesquisa";
+import { Fundo } from "../../Components/BarraPesquisa/Style";
+import { CartContext } from "../../Contexts/CartContext";
+import { Cards } from "../../Components/Cards/Cards";
+import axios from "axios";
 
-export const CarrinhoCompras =()=> {
+export const CarrinhoCompras = () => {
+  const { cart, addItem, removeItem, clearCart } = useContext(CartContext);
+  //teste
+  function jsonProduto() {
+    let daa = []
+    // daa = {
+    //   idCliente: 1,
+    //   itemPedidoList: [
+    //     {
+    //       produto: {
+    //         idProduto: 1,
+    //       },
+    //       quantidade: 1,
+    //       precoVenda: 5000.0,
+    //       percentualDesconto: 0.1,
+    //     },
+    //   ],
+    // };
+    for (let index = 0; index <= 1; index++) {
+      daa[index] = {
+        produto: {
+          idProduto: cart[index].idProduto,
+        },
+        quantidade: 1,
+        precoVenda: cart[index].valorUnitario,
+        percentualDesconto: 0.1,
+      };
+    }
 
-  // let item = useContext(CartContext).addItem
-  // item({
-  //     "nome":"The Last of Us",
-  //     "preco":199.90,
-  //     "quantidade":1,
-  //     "categoria":"playstation"
-  // })
-  // const [carrinho, setCarrinho] = useState(useContext(CartContext).cart) 
-  // console.log(carrinho)
-  const{ cart, addItem, removeItem, clearCart } = useContext(CartContext)
+    console.log(daa);
+    teste(daa);
+  }
+
+  function teste(daa) {
+    let haha = {
+      idCliente: 1,
+      itemPedidoList: [daa[1]],
+    };
+    console.log(haha);
+    axios.post("http://localhost:8080/ecommerce/pedido", haha);
+  }
 
   return (
-
     <div>
       <Fundo>
-        <BarraPesquisa/>
-        <h1>{cart}</h1>
-        
+        <BarraPesquisa />
+        {/* criar componente card, com componentes de diminuir ou aumentar quantidade e remover do carrinho */}
+        <Cards lista={cart} />
+        <button onClick={() => jsonProduto()}>dasda</button>
       </Fundo>
     </div>
-  )
-}
+  );
+};
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,38 +7,7 @@ import { CartContext } from "../Contexts/CartContext";
 import axios from "axios";
 
 export const Home = () => {
-  const [produtos, setProdutos] = useState([
-    // {
-    //   idProduto: 1,
-    //   nomeProduto: "Playstation 5",
-    //   descricaoProduto: "console de videogame",
-    //   qtdEstoque: 50,
-    //   valorUnitario: 5000.0,
-    //   dataCadastro: "19/11/2021",
-    //   nomeCategoria: "Eletronicos",
-    // },
-    // {
-    //   idProduto: 2,
-    //   nomeProduto: "Playstation 5",
-    //   descricaoProduto: "console de videogame",
-    //   qtdEstoque: 50,
-    //   valorUnitario: 5000.0,
-    //   dataCadastro: "19/11/2021",
-    //   nomeCategoria: "Eletronicos",
-    // },
-    // {
-    //   idProduto: 3,
-    //   nomeProduto: "macbook",
-    //   descricaoProduto: "Samsung Smart TV 50",
-    //   qtdEstoque: 1,
-    //   valorUnitario: 3000.0,
-    //   dataCadastro: "05/06/2022",
-    //   nomeCategoria: "Eletronicos",
-    // },
-  ]);
-
-  //teste
-  // const { cart, addItem, removeItem, clearCart } = useContext(CartContext);
+  const [produtos, setProdutos] = useState([]);
 
   useEffect(() => {
     buscarProdutosApi();
@@ -54,38 +23,7 @@ export const Home = () => {
         console.log(err);
       });
   }
-//teste
-  // function jsonProduto() {
-    
-  //   let daa = {
-  //     produto: {
-  //       idProduto: cart[0].idProduto,
-  //     },
-  //     quantidade: 1,
-  //     precoVenda: cart[0].valorUnitario,
-  //     percentualDesconto: 0.1,
-  //   };
-  //   cart.forEach(element => {
-  //     daa={
-  //     produto: {
-  //       idProduto: cart[0].idProduto,
-  //     },
-  //     quantidade: 1,
-  //     precoVenda: cart[0].valorUnitario,
-  //     percentualDesconto: 0.1,
-  //   };
-  //   });
-  //   teste(daa)
-  // }
 
-  // function teste(daa) {
-  //   let haha = {
-  //     idCliente: 1,
-  //     itemPedidoList: [daa],
-  //   };
-  //   console.log(haha);
-  //   axios.post("http://localhost:8080/ecommerce/pedido", haha);
-  // }
 
   return (
     <>
@@ -93,19 +31,6 @@ export const Home = () => {
         <BarraPesquisa />
 
         <Cards lista={produtos} />
-        {/* teste */}
-        {/* <button onClick={() => jsonProduto()}>dasda</button> */}
-        {/* {produtos.map((produto, index) => {
-        return (
-          <>
-            <p>{produto.nomeProduto}</p>
-            <p>{produto.descricaoProduto}</p>
-            <p>{produto.nomeCategoria}</p>
-          </>
-        );
-      })} */}
-
-        {/* <h1 onClick={()=>(item)}>{carrinho.nome}</h1> */}
       </Fundo>
     </>
   );
